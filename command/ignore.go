7004410cb89175b6956a62456d29d5a2b9042913
Chris Lee <chris.yx.lee@gmail.com>
2022-03-18 01:16:08 -0700

    ✨ Over-engineer newline detection for appending to ignore file

--- command/ignore.go
+++ command/ignore.go
@@ -3,6 +3,7 @@ package command
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
@@ -11,11 +12,64 @@ import (
 	"go.xargs.dev/bindl/internal"
 )
 
-func getIgnoreEntries(conf *config.Runtime) map[string]bool {
-	outdir := conf.OutputDir
-	return map[string]bool{
-		filepath.Join(outdir, "*"): false,
+func isNewline(c byte) bool {
+	return c == '\n' || c == '\r'
+}
+
+func getNumNewlinesRequired(f *os.File) (int, error) {
+	// Empty file?
+	off, err := f.Seek(0, os.SEEK_END)
+	if off == 0 || err == io.EOF {
+		return 0, nil
 	}
+	if err != nil {
+		return 0, err
+	}
+
+	// Check last character.
+	buf := make([]byte, 1)
+	if _, err = f.Seek(-1, os.SEEK_END); err != nil {
+		return 0, err
+	}
+	if _, err = f.Read(buf); err != nil {
+		return 0, err
+	}
+	if !isNewline(buf[0]) {
+		return 2, nil
+	}
+
+	// Check 2nd-to-last character.
+	if _, err = f.Seek(-2, os.SEEK_END); err != nil {
+		return 0, err
+	}
+	if _, err = f.Read(buf); err != nil {
+		return 0, err
+	}
+	if !isNewline(buf[0]) {
+		return 1, nil
+	}
+
+	// Already have 2 newlines. No need to format.
+	return 0, nil
+}
+
+func getValidTargets(outputDir string) map[string]bool {
+	noTrailingSlash := strings.TrimSuffix(outputDir, "/")
+	return map[string]bool{
+		filepath.Join(outputDir, "*"): true,
+		noTrailingSlash + "/":         true,
+		noTrailingSlash:               true,
+	}
+}
+
+func getIgnoreEntry(outputDir string, numPrefixNewlines int) string {
+	prefix := ""
+	for i := 0; i < numPrefixNewlines; i++ {
+		prefix += "\n"
+	}
+
+	internal.Log().Debug().Int("newlines", numPrefixNewlines).Msg("creating entry to add to file")
+	return prefix + "# Development and tool binaries\n" + filepath.Join(outputDir, "*") + "\n"
 }
 
 func UpdateIgnoreFile(conf *config.Runtime, path string) error {
@@ -27,32 +81,26 @@ func UpdateIgnoreFile(conf *config.Runtime, path string) error {
 	}
 	defer f.Close()
 
-	fc := map[string]bool{}
+	targets := getValidTargets(conf.OutputDir)
+
+	n := 0
 	scanner := bufio.NewScanner(f)
-	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
-		l := strings.Trim(scanner.Text(), "\n")
-		fc[l] = true
-		internal.Log().Debug().Str("line", l).Msg("parsing ignore file contents")
-	}
-
-	desired := getIgnoreEntries(conf)
-	for entry, _ := range desired {
-		if fc[entry] {
-			desired[entry] = true
+		n++
+		l := strings.TrimSpace(scanner.Text())
+		internal.Log().Debug().Int("line", n).Str("ignore", l).Msg("parsing ignore file content")
+		if targets[l] {
+			internal.Log().Info().Int("line", n).Msg("found a qualifying ignore file entry, not modifying")
+			return nil
 		}
 	}
 
-	for entry, presence := range desired {
-		if presence {
-			continue
-		}
-
-		_, err := f.WriteString(entry + "\n")
-		if err != nil {
-			return err
-		}
+	internal.Log().Debug().Msg("no qualifying ignore file entry found, adding")
+	numNewlinesRequired, err := getNumNewlinesRequired(f)
+	if err != nil {
+		return err
 	}
 
-	return nil
+	_, err = f.WriteString(getIgnoreEntry(conf.OutputDir, numNewlinesRequired))
+	return err
 }
